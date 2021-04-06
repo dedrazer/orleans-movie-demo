63bccf6ec0c01a2b96d6a30f398f9a05325be54f
Meli <martin.meli@qwkinsights.cloud>
2021-04-06 22:30:25 +0200

    get movie from JSON

--- Movies.Grains/MovieGrain.cs
+++ Movies.Grains/MovieGrain.cs
@@ -1,6 +1,10 @@
 ﻿using Movies.Contracts;
+using Newtonsoft.Json;
 using Orleans;
 using Orleans.Providers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Movies.Grains
@@ -8,8 +12,17 @@ namespace Movies.Grains
 	[StorageProvider(ProviderName = "Default")]
 	public class MovieGrain : Grain<MovieDataModel>, IMovieGrain
 	{
+		private const string _moviesJson = "movies.json";
+
 		public Task<MovieDataModel> Get()
-			=> Task.FromResult(State);
+		{
+			var moviesJsonString = File.ReadAllText(_moviesJson);
+
+			var movies = JsonConvert.DeserializeObject<IEnumerable<MovieDataModel>>(moviesJsonString);
+			var s = this.GetPrimaryKeyString();
+			var res = movies.FirstOrDefault(x => x.Id == this.GetPrimaryKeyString());
+			return Task.FromResult(res);
+		}
 
 		public Task Set(string name)
 		{
